2cd9d88307b2043e4c94114e6f670fb7b3c533a0
ruan.fachini <ruan.fachini@bcomex.com.br>
2021-02-08 22:18:16 -0300

    Criado teste de ordenação de colunas para exportação

---
+++ AutoOpenXmlTest/Cases/ExportOrderedProperties/ExportOrderedPropertiesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoOpenXml;
+using ClosedXML.Excel;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AutoOpenXmlTest.Cases.ExportOrderedProperties
+{
+    public class ExportOrderedPropertiesTest
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void ShouldExportExcelFileWithNumberColumn()
+        {
+            var stream = new AutoOpenXmlManager<ModelOrderedProperties>()
+                            .Init()
+                            .SetData(Variables.Data.ToList())
+                            .StartExportProcess();
+
+            var workbook = new XLWorkbook(stream);
+            workbook.TryGetWorksheet(Variables.WorksheetName, out var worksheet);
+
+            worksheet.Should().NotBeNull();
+
+            worksheet.Cell(1, 1).Value.ToString().Should().BeEquivalentTo(Variables.NameLabel);
+            worksheet.Cell(1, 2).Value.ToString().Should().BeEquivalentTo(Variables.IdLabel);
+            worksheet.Cell(1, 3).Value.ToString().Should().BeEquivalentTo(Variables.BirthDateLabel);
+            worksheet.Cell(1, 4).Value.ToString().Should().BeEquivalentTo(Variables.HeightLabel);
+        }
+    }
+}
---
+++ AutoOpenXmlTest/Cases/ExportOrderedProperties/ModelOrderedProperties.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoOpenXml;
+
+namespace AutoOpenXmlTest.Cases.ExportOrderedProperties
+{
+    [ExportWorkSheet(Variables.WorksheetName)]
+    internal class ModelOrderedProperties
+    {
+        [ExportColumn(Variables.IdLabel, 2, ColumnTypes.Int)]
+        public int Id { get; set; }
+        [ExportColumn(Variables.NameLabel, 1, ColumnTypes.Text)]
+        public string Name { get; set; }
+        [ExportColumn(Variables.HeightLabel, 4, ColumnTypes.Decimal)]
+        public decimal Height { get; set; }
+        [ExportColumn(Variables.BirthDateLabel, 3, ColumnTypes.DateTime)]
+        public DateTime BrithDate { get; set; }
+
+        public ModelOrderedProperties() { }
+        public ModelOrderedProperties(int id, string name, decimal height, DateTime brithDate)
+        {
+            Id = id;
+            Name = name;
+            Height = height;
+            BrithDate = brithDate;
+        }
+    }
+
+    internal static class Variables
+    {
+        public const string WorksheetName = "Nova Aba";
+        public const string NameLabel = "Nome";
+        public const string IdLabel = "Código";
+        public const string HeightLabel = "Altura";
+        public const string BirthDateLabel = "Aniversário";
+        public static readonly ModelOrderedProperties[] Data = {
+            new ModelOrderedProperties(1,"Carlos", (decimal)19.85, new DateTime(1991, 8, 25))
+        };
+    }
+}
